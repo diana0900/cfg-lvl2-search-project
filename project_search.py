e8022bd13e48e4f358305e2d0d08a249588a839b
diana0900 <63746261+diana0900@users.noreply.github.com>
2020-04-27 18:01:31 +0200

    Update project_search.py

--- project_search.py
+++ project_search.py
@@ -8,9 +8,16 @@ def recipe_search(ingredient, diet):
     return data['hits']
 
 def get_ingredient():
-    result = input('What ingredient would you like to cook?:')
-    return result
-
+    result = input("What do you have in the fridge today?")
+    potential_ingredient = input("Alright, is there anything else you would like to include on your ingredient list?(y/n)")
+    if potential_ingredient == "y":
+        additionals = input("What shall it be?")
+        result == result + potential_ingredient
+        print("Great, let's see what we can do with {} and {}!".format(result, additionals))
+    else:
+        print("Alright, looks like we can work with {}.".format(result))
+return result
+        
 def get_diet():
     done = False
     while not done:
